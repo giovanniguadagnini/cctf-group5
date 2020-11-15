4e389b80bfb9c5bb193a5944786ba20434974e00
Dupré Naïl <nail.dupre@gmail.com>
2020-11-15 20:58:37 +0100

    alternative packet collection through scapy, modified startup.sh accordiingly

--- resilient_cctf/blue_task/startup.sh
+++ resilient_cctf/blue_task/startup.sh
@@ -130,15 +130,21 @@ mkdir /home/cctf/snort
 chmod 777 /home/cctf/snort
 mkdir /home/cctf/snort/alerts
 chmod 777 /home/cctf/snort/alerts
-echo '#alert tcp any any -> 10.1.5.2 80 (msg: "TPC SYN detected"; sid: 1000003; rev: 1; flags:S;)' > /home/cctf/snort/snort.conf
-echo '#rate_filter gen_id 1, sig_id 1000003, track by_src, count 100, seconds 1, new_action drop, timeout 10' >> /home/cctf/snort/snort.conf
-echo '#event_filter gen_id 1, sig_id 1000003, track by_src, count 1, seconds 10, type limit' >> /home/cctf/snort/snort.conf
-echo '#drop tcp any any -> 10.1.5.2 80 (msg: "Detected junk traffic"; sid: 1000005;  pcre:"/.-. /";)' >> /home/cctf/snort/snort.conf
-echo '#event_filter gen_id 1, sig_id 1000005, track by_src, count 5, seconds 10, type limit' >> /home/cctf/snort/snort.conf
-echo '#alert tcp any any -> 10.1.5.2 80 (msg: "TCP SYN flood attack detected"; sid: 1000004; rev: 1; flags:S; detection_filter: track by_src, count 100, seconds 1;)' >> /home/cctf/snort/snort.conf
-echo '#event_filter gen_id 1, sig_id 1000004, track by_src, count 1, seconds 10, type limit' >> /home/cctf/snort/snort.conf
+echo 'alert tcp any any -> 10.1.5.2 80 (sid:1000001; msg:"test";)' > /home/cctf/snort/snort.conf
 echo 'config policy_mode:inline' >> /home/cctf/snort/snort.conf
 sudo snort --daq nfq -Q -c /home/cctf/snort/snort.conf -l /home/cctf/snort/alerts -D 
 exit
 EOF
 echo "[gateway] Enabled iptables rules in gateway machine, created folder /home/cctf and uploaded scripts"
+
+echo "[gateway] Setting up Scapy and alternative client stat collection"
+ssh $GATEWAY 1> /dev/null 2>>errors/startup_server.txt <<EOF
+cp -r resilient_cctf/red_task/lib /home/cctf
+sudo bash
+cd /home/cctf/lib
+tar -xzf scapy-2.4.4.tar.gz
+sudo mv /home/cctf/lib/scapy-2.4.4/ /home/cctf/scripts/scapy
+sudo apt-get install python-scapy
+exit
+EOF
+echo "[gateway] Set up of Scapy and alternative client stat collection done"
