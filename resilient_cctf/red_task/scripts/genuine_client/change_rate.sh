39428c951cb7ac912eb45406c573c2444a2ad36f
Giovanni <giovanni.guadagnini@gmail.com>
2020-11-16 11:27:34 +0100

    Changed genuine script (now the rate can be changed on-the-go creating rate.txt in the same folder), re-added basic snort rules in startup)

---
+++ resilient_cctf/red_task/scripts/genuine_client/change_rate.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+if [ "$#" -ne 2 ];
+then 
+    echo "Usage ./change_rate.sh <ip_genuine_client> <timeout>"
+    exit 1
+else
+    ssh $1 "echo $2 > /home/cctf/scripts/genuine_client/rate.txt" 1> /dev/null
+fi
