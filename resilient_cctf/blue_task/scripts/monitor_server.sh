ee56c1dc39fee71ef456ffe52861d619c9a3ee71
Giovanni <giovanni.guadagnini@gmail.com>
2020-11-17 12:20:59 +0100

    Monitor_server.sh run unitl it's stopped

--- resilient_cctf/blue_task/scripts/monitor_server.sh
+++ resilient_cctf/blue_task/scripts/monitor_server.sh
@@ -22,10 +22,12 @@ then
 REQUESTS=$2
 fi
 
+while true 
+do
 if [ "$1" == "1" ] || [ "$1" == "3" ];
 then
 echo "Start ping test (press ^C to interrupt)"
-sudo ping -i 0.1  $SERVER -c $REQUESTS
+sudo ping -i 0.1 $SERVER -c $REQUESTS
 fi
 
 if [ "$1" == "2" ] || [ "$1" == "3" ];
@@ -33,6 +35,10 @@ then
 echo "Start http server test"
 for ((i=0; i<$REQUESTS; i++))
 do
-curl -w "time_total: %{time_total}s\n" -o /dev/null -s $SERVER/1.html
+curl -w "time_total: %{time_total}s\n" -o /dev/null -s $SERVER/1.html 
 done
 fi
+
+sleep 5
+
+done
