ee27373ad02844ff510b08a2973e6b85eb988796
Giovanni <giovanni.guadagnini@gmail.com>
2020-11-26 12:57:39 +0100

    Added code to paginate the balance

--- secure_cctf/blue_task/change_default_password.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-
-sudo mysql -e "use ctf2; UPDATE users SET pass='RANDOMdaZilWk8WMdi' WHERE pass='abcdef'; "
--- secure_cctf/original_php_files/process_fixed.php
+++ secure_cctf/original_php_files/process_fixed.php
@@ -1,7 +1,14 @@
 <html>
-
+<head>
+    <title>Bank - Reserved Area</title>
+    <!-- Bootstrap CDN -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+</head>
 <body>
     <?php
+    //Open the file for the log operations
     $myFile = "/tmp/request.log";
     $fh = fopen($myFile, 'a');
 
@@ -67,9 +74,24 @@
 
         print "User $user successfully!";
         closeDBandFile('<script type="text/javascript">window.location.href="' . $url . '"; </script>');
-    } else if ($choice == 'balance' && checkUserCredentials($user, $pass) == true) {  
-        // Limit balance to the last 10 user operations
-        $stm = $mysqli->prepare("SELECT * FROM transfers WHERE user = ? ORDER BY id DESC LIMIT 10");
+    } else if ($choice == 'balance' && checkUserCredentials($user, $pass) == true) {
+
+        printUserInfo($user);  
+
+        //Check if pageno variable is setted
+        if (isset($_GET['pageno'])) {
+            $pageno = intval($_GET['pageno']);
+            if(gettype($pageno) != "integer"){
+                closeDBandFile("Impossible to use pagination with a non numeric index.");
+            }
+        } else {
+            $pageno = 1;
+        }
+        $no_of_records_per_page = 25;
+        $offset = ($pageno-1) * $no_of_records_per_page;
+
+        // Calculate the number of pages using the number of records
+        $stm = $mysqli->prepare("SELECT COUNT(*) as count FROM transfers WHERE user = ?");
         $stm->bind_param("s", $user);
 
         if ($stm->execute() == false){
@@ -77,11 +99,24 @@
         }
 
         $result = $stm->get_result();
+        $row = $result->fetch_array();
+        $total_rows = $row["count"]; 
+        $total_pages = ceil($total_rows / $no_of_records_per_page);
+
+        // Limit balance to the last 10 user operations
+        $stm2 = $mysqli->prepare("SELECT * FROM transfers WHERE user = ? LIMIT ?,?");
+        $stm2->bind_param("sii", $user, $offset, $no_of_records_per_page);
+
+        if ($stm2->execute() == false){
+            closeDBandFile($stm2->error); //In case of error print the error and close the connection with file and mysql 
+        }
+
+        $result2 = $stm2->get_result();
 
         $sum = 0;
-        print "<h1>Balance and transfer history of last 10 transactions for $user</h1>";
-        print "<table border=1><tr><th>Action</th><th>Amount</th></tr>";
-        while ($row = $result->fetch_array()) {
+        print "<h1>Balance and transfer history</h1>";
+        print "<table class='table'><thead><tr><th>Action</th><th>Amount</th></tr></thead><tbody>";
+        while ($row = $result2->fetch_array()) {
             $amount = $row['amount'];
             if ($amount < 0) {
                 $action = "Withdrawal";
@@ -90,10 +125,30 @@
             }
             print "<tr><td>" . $action . "</td><td>" . $amount . "</td></tr>";
         }
+        print "</tbody></table>";
 
-        print "<tr><td>Total</td><td>" . getUserBalance($user) . "</td></tr></table>";
+        $prev_page = $pageno - 1;
+        if($prev_page < 1){
+            $prev_page = 1;
+        }
+        $next_page = $pageno + 1;
+        print "<ul class='pagination'>";
+        print "<li><a href='$url&pageno=1'>First</a></li>";
+        if($prev_page >= 1){
+            print "<li><a href='$url&pageno=$prev_page'>Prev</a></li>";
+        }
+        if($next_page <= $total_pages){
+            print "<li><a href='$url&pageno=$next_page'>Next</a></li>";
+        }
+        print "<li><a href='$url&pageno=$total_pages'>Last</a></li>";
+        print "</ul>"; 
+
+        print "<b>User balance: " . getUserBalance($user) . "</b><br />";
+
         print "Back to <a href='index.php'>home</a>";
     } else if ($choice == 'deposit' && checkUserCredentials($user, $pass) == true) {
+
+        printUserInfo($user);
         
         // Check if the amount inserted from the user is positive, smaller than the maximum int in the db and if it's really and integer 
         if($amount < 0){
@@ -116,6 +171,8 @@
         print("Deposit of $amount for user $user completed successfully!");
         closeDBandFile('<script type="text/javascript">window.location.href="' . $url . '"; </script>');
     } else if ($choice == 'withdraw' && checkUserCredentials($user, $pass) == true) {
+
+        printUserInfo($user);
 
         // Check if the amount inserted from the user is positive, smaller than the maximum int in the db and if it's really and integer
         if($amount < 0){
@@ -151,6 +208,7 @@
         print("Withdraw of $amount for user $user completed successfully!");
         closeDBandFile('<script type="text/javascript">window.location.href="' . $url . '"; </script>');
     } else {
+        printUserInfo($user);
         print "The specified operation is not allowed!";
         closeDBandFile('<script type="text/javascript">window.location.href="' . $url . '"; </script>');
     }
@@ -173,6 +231,14 @@
     // Close connection to file and mysql
     $mysqli->close();
     fclose($fh);
+
+    /*
+    * Print welcome message when user is registered 
+    * $user username to set in the message
+    */
+    function printUserInfo($user){
+        print "<h1>Welcome $user</h1><br />";
+    }
 
     /*
     * Close the connection to file and mysql if an error or something bad happen
