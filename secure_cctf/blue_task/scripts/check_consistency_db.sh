6254a267a9df01873d689c7a4d8595cf5b137a25
Giovanni <giovanni.guadagnini@gmail.com>
2020-11-26 18:36:56 +0100

    Fixed table engine and added script to check consistency in the database

---
+++ secure_cctf/blue_task/scripts/check_consistency_db.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+FILENAME1=test_consitency1.txt
+FILENAME2=test_consitency2.txt
+
+echo "Check db consistency is running ..."
+COUNT=1
+while true
+do
+    echo "SELECT user, SUM(amount) AS balance FROM ctf2.transfers GROUP BY (user) HAVING balance < 0;" | mysql -u"root" -p"rootmysql" 1>$FILENAME1 2> /dev/null
+    echo "SELECT user FROM ctf2.transfers WHERE user NOT IN (SELECT user FROM ctf2.users) GROUP BY user;" | mysql -u"root" -p"rootmysql" 1>$FILENAME2 2> /dev/null
+    if [ -s $FILENAME1 ]
+    then
+        echo "ATTENTION! Detected user/s with negative balance! ($COUNT)"
+        cat $FILENAME1
+    fi
+    if [ -s $FILENAME2 ]
+    then
+        echo "ATTENTION! Detected transaction of a user/s not inserted in the user table! ($COUNT)"
+        cat $FILENAME2
+    fi
+
+    if [ ! -s $FILENAME1 ] && [ ! -s $FILENAME2 ]
+    then
+        echo "No consistency problem detected! ($COUNT)"
+    fi
+
+    sleep 5;
+    rm -rf $FILENAME1
+    rm -rf $FILENAME2
+    echo "-------------------------------"
+    COUNT=$((COUNT+1))
+done
